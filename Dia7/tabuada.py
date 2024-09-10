affe00db87e9cd12ba2a43b026819caaacf5b74f
ArthurMorais0 <arthur162016@gmail.com>
2024-09-10 16:05:44 -0300

    Tabuda de um numero em Python

---
+++ Dia7/tabuada.py
@@ -0,0 +1,8 @@
+# Numero referente
+numero = int(input("Digite um número para ver a tabuada: "))
+
+# Exibe a tabuada de 1 a 10
+print(f"Tabuada de {numero}:")
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
