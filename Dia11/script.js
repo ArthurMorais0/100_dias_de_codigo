6e8240ba6da5660b973dd73b013ef0175d8c293d
ArthurMorais0 <arthur162016@gmail.com>
2024-09-14 16:19:55 -0300

    botao interativo com css e js

---
+++ Dia11/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+
+// Adiciona um evento de clique
+btn.addEventListener("click", function() {
+    // Alterna a classe 'mudado' no botão
+    btn.classList.toggle("changed");
+});
