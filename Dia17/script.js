186d9ec8934267b46ba87879968a021787b79e83
ArthurMorais0 <arthur162016@gmail.com>
2024-09-20 15:33:06 -0300

    Gerador de senha aleatorio

---
+++ Dia17/script.js
@@ -0,0 +1,14 @@
+document.getElementById('generate-btn').addEventListener('click', generatePassword);
+
+function generatePassword() {
+    const length = document.getElementById('password-length').value;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById('generated-password').value = password;
+}
