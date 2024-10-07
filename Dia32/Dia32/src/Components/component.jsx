87c4c0f1c854224f56668d44d5eba0f8b35d7127
ArthurMorais0 <arthur162016@gmail.com>
2024-10-06 21:20:21 -0300

    Components no React

---
+++ Dia32/Dia32/src/Components/component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      <h1>Olá este é um ExampleComponent!</h1>
+      <p>Exemplo de Componente</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
